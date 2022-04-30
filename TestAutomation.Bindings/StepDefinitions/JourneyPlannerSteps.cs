6dbe91727a06a8d349c8a22993c009c3e02e794e
Olu <olu.raji@yahoo.co.uk>
2022-04-30 22:52:11 +0100

    add step definition

---
+++ TestAutomation.Bindings/StepDefinitions/JourneyPlannerSteps.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestAutomation.Bindings.StepDefinitions
+{
+    internal class JourneyPlannerSteps
+    {
+    }
+}
